9daa5c735e93473b20ebfb73097b05b16cc4a599
Srikanth <sri1@publicisgroupe.net>
2022-02-08 14:31:19 +0530

    navbar styles added

--- src/Routes/ApplicationRoutes.js
+++ src/Routes/ApplicationRoutes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import BlogCreatePage from "../Pages/BlogCreatePage";
+import Navbar from '../components/Navbar';
 
 const ApplicationRoutes = () => {
     return <>
+        <Navbar />
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/createblog" element={<BlogCreatePage />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Navbar = () => {
     return <Wrapper>
-
+        <div className="nav-container section-center">
+            <Link to="/" className="logo-container">
+                <img src="https://racharlasrikanth.netlify.app/static/media/logo.aa78497e534416d460cb8eac1661bee4.svg" alt="" />
+            </Link>
+            <div className="nav-links">
+                <ul className="links">
+                    <li className="each-link">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="each-link">
+                        <Link to="/createblog">Create Blog</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </Wrapper>
 }
 
 const Wrapper = styled.nav`
+    background: var(--clr-primary-10);
+    min-height: 10vh;
+    display: grid;
+    place-items: center;
+    width: 100%;
 
+    .nav-container{
+        width: 80%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .logo-container{
+        max-width: 70px;
+        max-height: 70px;
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+    .nav-links .links{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        column-gap: 1rem;
+
+        a{
+            color: var(--clr-primary-1);
+            font-size: 1rem;
+            transition: var(--transition);
+            letter-spacing: var(--spacing);
+
+            :hover{
+                color: var(--clr-primary-3);
+            }
+        }
+    }
 `
 
 export default Navbar;
